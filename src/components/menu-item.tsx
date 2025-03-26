5ac82d50ebaff635319dc489ac1d34b49d99360d
Laura Cintra <lauraocintra@gmail.com>
2025-03-25 22:04:59 -0300

    Adicionando transição menu

--- src/components/menu-item.tsx
+++ src/components/menu-item.tsx
@@ -7,9 +7,9 @@ interface MenuItemProps {
 
 export default function MenuItem({ icon: Icon, text }: MenuItemProps){
   return (
-    <div className="flex items-center gap-3 p-3 text-slate-950 hover:bg-gray-100 rounded-lg">
-      <Icon className="text-xl" />
-      <p className="text-sm">{text}</p>
+    <div className="flex items-center text-slate-950 hover:bg-gray-100 rounded-lg p-2">
+      <Icon className="text-2xl mx-4" />
+      <p className="text-2sm">{text}</p>
     </div>
   );
 };
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-// import { useState } from "react";
+import { useState } from "react";
 import Logo from "../app/img/logo.png";
 import { BellRing, ChevronDown, NotebookPen, Plus, TrendingUp } from "lucide-react";
 import MenuItem from "./menu-item";
@@ -15,22 +15,41 @@ const menuItems = [
 
 export default function NavBar() {
 
+  // Estado para controlar se o menu está expandido ou minimizado
+  const [isMenuCollapsed, setIsMenuCollapsed] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuCollapsed(!isMenuCollapsed);
+  };
+
   return (
     <>
-        <nav className="flex flex-col gap-2 bg-white max-w-3xs h-screen items-center justify-between p-6">
+        <nav className={`flex flex-col gap-2 bg-white max-w-3xs h-screen items-center justify-between p-6 drop-shadow-lg transition-all duration-300 ${
+        isMenuCollapsed ? "w-20" : "w-72"}`}>
           <div className="flex flex-col items-center gap-2">
-          <Image src={Logo} alt="" width="100" height="50" />
-          <hr />
-          <div className="justify-start">
-          {menuItems.map((item, index) => (
-            <MenuItem key={index} icon={item.icon} text={item.text}/>
-          ))}
-          </div>
+            <div
+            className="cursor-pointer"
+            onClick={toggleMenu}
+            >
+            <Image
+              src={Logo}
+              alt="Logo"
+              width={isMenuCollapsed ? 70 : 90}
+              height={isMenuCollapsed ? 35 : 40}/>
+            </div>
+            
+            <div className={`bg-gray-400 h-0.5 ${isMenuCollapsed ? "w-20" : "w-62"} m-3`}></div>
+          
+            <div className="flex flex-col gap-6 mt-4">
+            {menuItems.map((item, index) => (
+              <MenuItem key={index} icon={item.icon} text={isMenuCollapsed ? "" : item.text}/>
+            ))}
+            </div>
           </div>
           <div className="flex gap-2 items-center text-slate-950">
-          <img className="size-12 rounded-full" src="https://github.com/Laura-Cintra.png" alt="" />
-          <p>Laura Cintra</p>
-          <ChevronDown/>
+          <img className="size-12 rounded-full" src="https://github.com/Laura-Cintra.png" alt="usuário" />
+          {!isMenuCollapsed && <p>Laura Cintra</p>} 
+          {!isMenuCollapsed && <ChevronDown/>}
           </div>
         </nav>
     </>
